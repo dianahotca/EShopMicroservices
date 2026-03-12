9dbc996c2289e536eb4064ef5446cee2c241b130
Diana Hotca <dianahotca10@gmail.com>
2026-03-12 14:59:20 +0200

    Create common interfaces

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //when no response is returned
+    public interface ICommandHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    {
+    }
+
+    //when response is returned
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse>
+    {
+    }
+}
