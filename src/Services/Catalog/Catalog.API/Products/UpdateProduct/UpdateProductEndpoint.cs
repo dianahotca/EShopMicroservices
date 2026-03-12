731ea0f5d448ec701b1d68c127a72b5862120ea7
Diana Hotca <dianahotca10@gmail.com>
2026-03-12 15:14:49 +0200

    Change PUT endpoint to accept id in the url

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -3,16 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Products.UpdateProduct
 {
-    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record UpdateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
     public record UpdateProductResponse(bool IsSuccess);
 
     public class UpdateProductEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            app.MapPut("/products/{id}", async (Guid id, UpdateProductRequest request, ISender sender) =>
             {
-                var command = request.Adapt<UpdateProductCommand>();
+                var command = new UpdateProductCommand(id, request.Name, request.Category, request.Description, request.ImageFile, request.Price);
                 var result = await sender.Send(command);
                 var response = result.Adapt<UpdateProductResponse>();
 
