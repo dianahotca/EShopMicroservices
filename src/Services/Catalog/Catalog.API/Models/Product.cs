fada6b9cdb9a41ce541541956ebbd15d25cd285b
Diana Hotca <dianahotca10@gmail.com>
2026-03-12 14:59:55 +0200

    Add Product model and CreateProduct endpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+        //1->N relationship
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
