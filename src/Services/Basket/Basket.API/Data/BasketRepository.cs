8b3ff76175d695e7c58329fcb9b66020a3b3147c
Diana Hotca <dianahotca10@gmail.com>
2026-03-13 17:19:32 +0200

    Implement basket repository

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace Basket.API.Data
+{
+    public class BasketRepository
+        (IDocumentSession session)
+        : IBasketRepository
+    {
+        public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken = default)
+        {
+            session.Delete<ShoppingCart>(username);
+            await session.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken = default)
+        {
+            var basket = await session.LoadAsync<ShoppingCart>(username, cancellationToken);
+
+            if(basket is null)
+            {
+                throw new BasketNotFoundException(username);
+            }
+
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken = default)
+        {
+            session.Store(cart);
+            await session.SaveChangesAsync();
+
+            return cart;
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken = default);
+        Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken = default);
+        Task<bool> DeleteBasket(string username, CancellationToken cancellationToken = default);
+    }
+}
