88553aa592675e4c9a12c4123f9af394b2813ce8
Diana Hotca <dianahotca10@gmail.com>
2026-03-13 16:03:46 +0200

    Setup get basket handler and create global using file

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //TODO: get basket from database
+            //var basket = await _respository.GetBasket(request.Username);
+
+            return new GetBasketResult(new ShoppingCart("smone"));
+        }
+    }
+}
