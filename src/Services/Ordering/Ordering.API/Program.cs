8f9e336f7a7c893a6eb011b98912020b3bd4c53e
Diana Hotca <dianahotca10@gmail.com>
2026-03-20 15:48:47 +0200

    Add database automigrate extension method for development

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,6 @@
 using Ordering.API;
 using Ordering.Application;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,5 +14,10 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Ordering.Infrastructure.Data.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        public static async Task InitializeDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DbContext>();
+
+            context.Database.MigrateAsync().GetAwaiter().GetResult();
+        }
+    }
+}
