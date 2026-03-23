86e8da6d74328c314f210132e199a63f154efe2b
Diana Hotca <dianahotca10@gmail.com>
2026-03-23 19:16:01 +0200

    Make argument camelcase

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -1,5 +1,5 @@
 ﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
 {
     public record GetOrdersByNameQuery(string Name) : IQuery<GetOrdersByNameResult>;
-    public record GetOrdersByNameResult(IEnumerable<OrderDto> orders);
+    public record GetOrdersByNameResult(IEnumerable<OrderDto> Orders);
 }
