858a10d9a7ff50543eb3150e7b77d8c628812fda
Diana Hotca <dianahotca10@gmail.com>
2026-03-23 15:07:22 +0200

    Update global imports

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,5 +1,7 @@
 ﻿global using FluentValidation;
 global using BuildingBlocks.CQRS;
 global using Ordering.Application.DTOs;
+global using Ordering.Application.Data;
+global using Ordering.Application.Exceptions;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
