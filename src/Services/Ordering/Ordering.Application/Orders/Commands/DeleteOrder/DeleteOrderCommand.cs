2be5ef28f88df965df51d7a36ec483808d23b95c
Diana Hotca <dianahotca10@gmail.com>
2026-03-23 15:16:56 +0200

    Implemente delete order command and handler

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteOrderCommand(Guid orderId) : ICommand<DeleteOrderResult>;
+    public record DeleteOrderResult(bool IsSuccess);
+
+    public class DeleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DeleteOrderCommandValidator()
+        {
+            RuleFor(deleteOrderCommand => deleteOrderCommand.orderId).NotEmpty().WithMessage("OrderId is required");
+        }
+}
