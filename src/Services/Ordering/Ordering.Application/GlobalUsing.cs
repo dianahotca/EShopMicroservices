33f5d1d9fad2c3a3ba38b3989467774e4a3567f6
Diana Hotca <dianahotca10@gmail.com>
2026-03-23 17:26:20 +0200

    Add domain events handlers for order created and order updated

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,7 +1,14 @@
-﻿global using FluentValidation;
+﻿global using MediatR;
+global using FluentValidation;
+global using Microsoft.Extensions.Logging;
+
 global using BuildingBlocks.CQRS;
+
 global using Ordering.Application.DTOs;
 global using Ordering.Application.Data;
 global using Ordering.Application.Exceptions;
+
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Events;
+
