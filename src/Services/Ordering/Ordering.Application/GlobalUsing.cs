ef4b69faf072dfa4a49e1ea753d87d5825e82143
Diana Hotca <dianahotca10@gmail.com>
2026-03-23 19:04:35 +0200

    Implement get orders by customer query and handler

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -8,6 +8,7 @@ global using BuildingBlocks.CQRS;
 global using Ordering.Application.DTOs;
 global using Ordering.Application.Data;
 global using Ordering.Application.Exceptions;
+global using Ordering.Application.Extensions;
 
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrdersByCustomerHandler
+        (IApplicationDbContext dbContext)
+        : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            var orders = await dbContext.Orders
+                .Include(order => order.OrderItems)
+                .AsNoTracking()
+                .Where(order => order.CustomerId == CustomerId.Of(query.CustomerId))
+                .OrderBy(order => order.OrderName.Value)
+                .ToListAsync(cancellationToken);
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
