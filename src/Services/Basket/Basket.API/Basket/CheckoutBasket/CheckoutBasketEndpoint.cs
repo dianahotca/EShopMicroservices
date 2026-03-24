185fccc498f637c3cd8b3b32d32e342381b9d472
Diana Hotca <dianahotca10@gmail.com>
2026-03-24 19:28:07 +0200

    Add missing await

--- src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketEndpoint.cs
@@ -10,7 +10,7 @@
             app.MapPost("/basket/checkout", async (CheckoutBasketRequest request, ISender sender) =>
             {
                 var command = request.Adapt<CheckoutBasketCommand>();
-                var result = sender.Send(command);
+                var result = await sender.Send(command);
                 var response = result.Adapt<CheckoutBasketResponse>();
 
                 return Results.Ok(response);
