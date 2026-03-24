fc49ae32a4bb9d701eb57079759ac0a919b3e32f
Diana Hotca <dianahotca10@gmail.com>
2026-03-24 19:27:54 +0200

    Create separate order item dtos for create/update and get

--- src/Services/Ordering/Ordering.Application/DTOs/OrderItemDto.cs
+++ src/Services/Ordering/Ordering.Application/DTOs/OrderItemDto.cs
@@ -1,9 +1,15 @@
 ﻿namespace Ordering.Application.DTOs
 {
     public record OrderItemDto(
-        Guid OrderId,
         Guid ProductId,
         int Quantity,
         decimal Price
         );
+
+    public record OrderItemResponseDto(
+     Guid OrderId,
+     Guid ProductId,
+     int Quantity,
+     decimal Price
+     );
 }
