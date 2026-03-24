d5ef02e8e94ca6639fdf91948874a1e5f279bbd8
Diana Hotca <dianahotca10@gmail.com>
2026-03-24 18:39:48 +0200

    Implement basket checkout endpoint and handler

---
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -0,0 +1,43 @@
+﻿
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
+namespace Basket.API.Basket.CheckoutBasket
+{
+    public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckoutDto) : ICommand<CheckoutBasketResult>;
+    public record CheckoutBasketResult(bool IsSuccess);
+
+    public class CheckoutBasketCommandValidator
+    : AbstractValidator<CheckoutBasketCommand>
+    {
+        public CheckoutBasketCommandValidator()
+        {
+            RuleFor(x => x.BasketCheckoutDto).NotNull().WithMessage("BasketCheckoutDto can't be null");
+            RuleFor(x => x.BasketCheckoutDto.Username).NotEmpty().WithMessage("Username is required");
+        }
+    }
+
+    public class CheckoutBasketHandler 
+        (IBasketRepository repository, IPublishEndpoint publishEndpoint)
+        : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+    {
+        public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+        {
+            var username = command.BasketCheckoutDto.Username;
+            var basket = await repository.GetBasket(username, cancellationToken);
+
+            if(basket == null)
+            {
+                return new CheckoutBasketResult(false);
+            }
+
+            var eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
+            eventMessage.TotalPrice = basket.TotalPrice;    //for security, don't rely on the value coming from the client
+            await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+            await repository.DeleteBasket(username, cancellationToken);
+
+            return new CheckoutBasketResult(true);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/GlobalUsing.cs
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -3,9 +3,12 @@ global using Marten;
 global using MediatR;
 global using Mapster;
 global using FluentValidation;
+
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
 global using BuildingBlocks.Exceptions;
+
 global using Basket.API.Models;
 global using Basket.API.Exceptions;
 global using Basket.API.Data;
+global using Basket.API.DTOs;
